1c0c3e57fd12869ae174543eab7c03bde65bafd6
unknown <curtisdslone@gmail.com>
2024-06-18 15:08:19 +0200

    CreateReservation Object gui

--- week6/ReservationManagerGUI/src/com/gui/ReservationBase.java
+++ week6/ReservationManagerGUI/src/com/gui/ReservationBase.java
@@ -1,6 +1,35 @@
 package com.gui;
 import javax.swing.*;
 
-public class ReservationBase extends  JPanel {
+public class ReservationBase extends JPanel {
+
+    JLabel type;
+    JLabel dd;
+    JLabel mm;
+    JLabel yyyy;
+    JLabel numNights;
+    JLabel numBeds;
+    JLabel numRooms;
+    JLabel numBaths;
+    JLabel lodgingSize;
+
+    JTextField ddTextField;
+    JTextField mmTextField;
+    JTextField yyyyTextField;
+    JTextField numNightsTextField;
+    JTextField numBedsTextField;
+    JTextField numRoomsTextField;
+    JTextField numBathsTextField;
+    JTextField lodgingSizTextField;
+
+    JRadioButton hotelButton;
+    JRadioButton houseButton;
+    JRadioButton cabinButton;
+
+    ButtonGroup typeButtonGroup;
+
+    public ReservationBase(){
+
+    }
 
 }
