31b7e35f33d5365d185980f7f7cab7b6b617179a
Curtis Slone <curtisdslone@gmail.com>
2024-06-19 22:05:22 +0200

    Enough GUI

--- week6/ReservationManagerGUI/src/App.java
+++ week6/ReservationManagerGUI/src/App.java
@@ -3,7 +3,6 @@ import com.gui.PrimaryWindow;
 
 public class App {
     public static void main(String[] args) throws Exception {
-       PrimaryWindow app = new PrimaryWindow();
-       app.setVisible(true);
+      
     }
 }
